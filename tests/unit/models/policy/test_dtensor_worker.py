bf08022acec1dc0c9780eb326eadf5a25ddb2cbe
Jonas yang <joyang@nvidia.com>
2025-06-16 14:18:26 +0800

    Fix up gemma3 in unittest.
    Signed-off-by: Jonas yang <joyang@nvidia.com>

--- tests/unit/models/policy/test_dtensor_worker.py
+++ tests/unit/models/policy/test_dtensor_worker.py
@@ -455,8 +455,6 @@ def logprob_setup(request, two_gpu_virtual_cluster):
         (TEST_ASSETS.TINY_LLAMA_MODEL_PATH, 1, 2, False, True, True),
         (TEST_ASSETS.TINY_QWEN3_MODEL_PATH, 1, 2, False, True, False),
         (TEST_ASSETS.TINY_QWEN3_MODEL_PATH, 1, 2, False, False, False),
-        (TEST_ASSETS.TINY_GEMMA3_MODEL_PATH, 1, 2, False, True, False),
-        (TEST_ASSETS.TINY_GEMMA3_MODEL_PATH, 1, 2, False, False, False),
     ],
     indirect=True,
 )
