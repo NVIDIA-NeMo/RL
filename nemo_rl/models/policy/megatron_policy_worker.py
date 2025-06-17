09d4470629999fd43b96c4327e13c9de0ee284a6
Sahil Jain <sahilj@nvidia.com>
2025-06-16 17:57:48 -0700

    lint
    Signed-off-by: Sahil Jain <sahilj@nvidia.com>

--- nemo_rl/models/policy/megatron_policy_worker.py
+++ nemo_rl/models/policy/megatron_policy_worker.py
@@ -614,7 +614,7 @@ class MegatronPolicyWorker:
         self.megatron_to_hf_converter = MegatronToHFConverter(hf_model_name, self.model)
 
     def configure_worker(self, num_gpus: int, bundle_indices: Optional[tuple] = None):
-        USE_EXPANDABLE_SEGMENTS = False # Disabling this right now as it seems to cause vLLM refit issues with Ampere
+        USE_EXPANDABLE_SEGMENTS = False  # Disabling this right now as it seems to cause vLLM refit issues with Ampere
         if USE_EXPANDABLE_SEGMENTS:
             return None, {"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"}, None
         else:
