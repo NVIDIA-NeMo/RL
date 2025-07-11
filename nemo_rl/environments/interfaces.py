ccb0e4cf56d2857afb03d20fca346c913d433dbc
ruit <ruit@nvidia.com>
2025-07-11 06:41:57 +0000

    move extracted_answer from EnvironmentReturn to EnvironmentReturn.metadata
    Signed-off-by: ruit <ruit@nvidia.com>

--- nemo_rl/environments/interfaces.py
+++ nemo_rl/environments/interfaces.py
@@ -40,7 +40,6 @@ class EnvironmentReturn(NamedTuple):
     metadata: list[Optional[dict]]
     next_stop_strings: list[list[str] | None] | list[None]
     rewards: Tensor
-    extracted_answers: list[str | None]
     terminateds: Tensor
 
 
