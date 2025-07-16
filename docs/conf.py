804ae1c24f5e187a42f21f9c08cc883d6e01d755
Terry Kong <terryk@nvidia.com>
2025-07-15 20:28:22 -0700

    Revert "purposefully add a typing error"
    This reverts commit 9472d7f192e6c7be569213db40ac68123b4a69a6.
    
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -40,13 +40,6 @@ extensions = [
     "sphinx_copybutton",  # For copy button in code blocks
 ]
 
-
-def foo(int: 4):
-    return
-
-
-foo("qwe")
-
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
