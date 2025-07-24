5cd9519ae41682567fb22e24eaf264d122b8eb95
Jimmy Zhang <jiemingz@nvidia.com>
2025-07-23 20:51:36 -0700

    fix TP and async engine
    Signed-off-by: Jimmy Zhang <jiemingz@nvidia.com>

--- nemo_rl/models/generation/vllm_backend.py
+++ nemo_rl/models/generation/vllm_backend.py
@@ -26,7 +26,6 @@ except ImportError:
         "If you are working interactively, you can install by running  `uv sync --extra vllm` anywhere in the repo."
     )
 
-
 class VllmInternalWorkerExtension:
     def init_collective(
         self, rank_prefix: int, ip: str, port: int, world_size: int
