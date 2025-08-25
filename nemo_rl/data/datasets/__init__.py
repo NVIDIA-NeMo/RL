51d02830f50d7ab6f11ee76f5fe4e83bba28a0fe
Yuki Huang <yukih@nvidia.com>
2025-08-25 14:43:28 +0000

    move eval and preference
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

---
+++ nemo_rl/data/datasets/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from nemo_rl.data.datasets.collate_fn import (
+    dpo_collate_fn,
+    eval_collate_fn,
+    preference_collate_fn,
+    rl_collate_fn,
+)
+from nemo_rl.data.datasets.eval_datasets import load_eval_dataset
+from nemo_rl.data.datasets.preference_datasets import load_preference_dataset
+from nemo_rl.data.datasets.processed_dataset import AllTaskProcessedDataset
+from nemo_rl.data.datasets.utils import assert_no_double_bos
+
+__all__ = [
+    "AllTaskProcessedDataset",
+    "dpo_collate_fn",
+    "eval_collate_fn",
+    "preference_collate_fn",
+    "rl_collate_fn",
+    "load_eval_dataset",
+    "load_preference_dataset",
+    "assert_no_double_bos",
+]
---
+++ nemo_rl/data/datasets/utils.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Union
+
+import torch
+from transformers import AutoProcessor, PreTrainedTokenizerBase
+
+TokenizerType = Union[PreTrainedTokenizerBase, AutoProcessor]
+
+
+def assert_no_double_bos(token_ids: torch.Tensor, tokenizer: TokenizerType) -> None:
+    """Assert that there are no double starting BOS tokens in the message.
+
+    Args:
+        token_ids: List of token IDs
+        tokenizer: Tokenizer
+    """
+    if tokenizer.bos_token_id is not None:
+        token_ids_list = token_ids.tolist()
+        if len(token_ids_list) > 1:
+            assert not (
+                token_ids_list[0] == tokenizer.bos_token_id
+                and token_ids_list[1] == tokenizer.bos_token_id
+            ), "Found double BOS token in the first two positions of the message."
+    else:
+        # `name_or_path` is not available for AutoProcessor, temp fix in get_tokenizer
+        print(
+            f"skip assert_start_single_bos since Tokenizer {tokenizer.name_or_path} has no BOS token"
+        )
