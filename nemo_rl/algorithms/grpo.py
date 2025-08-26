b2f0ed1d8fb0cae6d357d66b6e5f543e8b595949
Brian Yu <bxyu@nvidia.com>
2025-08-26 11:14:29 -0700

    add validation

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -413,7 +413,17 @@ def _should_use_penguin(master_config: MasterConfig) -> bool:
     """Determine if Penguin should be used for rollouts and validation based on the configuration.
     """
     env_config = master_config["env"]
-    return bool(env_config.get("should_use_penguin"))
+    should_use_penguin = bool(env_config.get("should_use_penguin"))
+    if not should_use_penguin:
+        return should_use_penguin
+
+    # Validate the setup for training with Penguin
+    assert _should_use_async_rollouts(master_config), f"In order to use Penguin, you must use vllm generation backend with `async_engine: true`!"
+    # We piggyback off of `_should_use_async_rollouts` to guarantee the existence of these configs.
+    should_expose_http_server = master_config["policy"]["generation"]["vllm_cfg"].get("expose_http_server")
+    assert should_expose_http_server, f"In order to use Penguin, you must expose the vllm server via `expose_http_server: true`!"
+
+    return should_use_penguin
 
 
 def refit_policy_generation(
