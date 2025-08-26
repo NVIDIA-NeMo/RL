3b7fc5b633f6033a6228648bf5cb7226d3cfaf2c
Brian Yu <bxyu@nvidia.com>
2025-08-26 12:28:47 -0700

    refactor penguin env

--- nemo_rl/environments/penguin.py
+++ nemo_rl/environments/penguin.py
@@ -22,6 +22,8 @@ import ray
 from nemo_rl.distributed.virtual_cluster import PY_EXECUTABLES
 from nemo_rl.environments.interfaces import EnvironmentInterface
 
+from nemo_rl.distributed.virtual_cluster import _get_node_ip_and_free_port
+
 
 class PenguinConfig(TypedDict):
     model_name: str
@@ -30,23 +32,41 @@ class PenguinConfig(TypedDict):
 
 
 @ray.remote
-def start_penguin(cfg: PenguinConfig, nemo_rl_openai_base_url: str):
-    # TODO we should probably rename this somehow to penguin. But that is a lot of work...
-    from nemo_gym.cli import run
+class PenguinWorker:
+    def __init__(self, cfg: PenguinConfig, nemo_rl_openai_base_url: str):
+        self.cfg = cfg
 
-    RELATIVE_PATH = "nemo_rl/environments/penguin.py"
-    assert __file__.endswith(RELATIVE_PATH)
+        self.nemo_rl_openai_base_url = nemo_rl_openai_base_url
+        self.node_ip, self.head_server_port = _get_node_ip_and_free_port()
 
-    initial_global_config_dict = cfg["initial_global_config_dict"]
-    with open_dict(initial_global_config_dict):
-        initial_global_config_dict["policy_model_name"] = cfg["model_name"]
-        initial_global_config_dict["policy_api_key"] = "dummy_key"  # No key necessary for training.
-        initial_global_config_dict["policy_base_url"] = nemo_rl_openai_base_url
+    def get_node_ip_header_server_port(self) -> tuple[str, int]:
+        return self.node_ip, self.head_server_port
 
-    run(
-        dotenv_path=Path(__file__.removesuffix(RELATIVE_PATH)).absolute() / "env.yaml",
-        initial_global_config_dict=initial_global_config_dict,
-    )
+    def start(self) -> None:
+        # TODO we should probably rename this somehow to penguin. But that is a lot of work...
+        from nemo_gym.cli import run
+        from nemo_gym.server_utils import HEAD_SERVER_KEY_NAME
+
+        RELATIVE_PATH = "nemo_rl/environments/penguin.py"
+        assert __file__.endswith(RELATIVE_PATH)
+
+        initial_global_config_dict = self.cfg["initial_global_config_dict"]
+        with open_dict(initial_global_config_dict):
+            # Policy information
+            initial_global_config_dict["policy_model_name"] = self.cfg["model_name"]
+            initial_global_config_dict["policy_api_key"] = "dummy_key"  # No key necessary for training.
+            initial_global_config_dict["policy_base_url"] = self.nemo_rl_openai_base_url
+
+            # Head server
+            initial_global_config_dict[HEAD_SERVER_KEY_NAME] = {
+                "host": "0.0.0.0",
+                "port": self.head_server_port,
+            }
+
+        run(
+            dotenv_path=Path(__file__.removesuffix(RELATIVE_PATH)).absolute() / "env.yaml",
+            initial_global_config_dict=initial_global_config_dict,
+        )
 
 
 @ray.remote(max_restarts=-1, max_task_retries=-1)  # pragma: no cover
@@ -65,7 +85,18 @@ class Penguin(EnvironmentInterface):
             for _ in range(self.cfg["base_urls"])
         ]
 
+        self.head_server_configs = []
+        self.start_tasks = []
+        for worker in self.workers:
+            node_ip, head_server_port = ray.get(worker.get_node_ip_header_server_port.remote())
+            self.head_server_configs.append({"host": node_ip, "port": head_server_port})
+
+            self.start_tasks.append(worker.start.remote())
+
     def shutdown(self) -> None:
+        for start_task in self.start_tasks:
+            ray.cancel(start_task)
+
         # shutdown all workers
         for worker in self.workers:
             ray.kill(worker)
