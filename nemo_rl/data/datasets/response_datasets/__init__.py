caf2cc05fcd74b724b3a224ca25d60576c18e667
Yuki Huang <yukih@nvidia.com>
2025-09-01 03:38:50 -0700

    refactor and move response datasets
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

---
+++ nemo_rl/data/datasets/response_datasets/__init__.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any
+
+from nemo_rl.data.datasets.response_datasets.clevr import CLEVRCoGenTDataset
+from nemo_rl.data.datasets.response_datasets.deepscaler import DeepScalerDataset
+from nemo_rl.data.datasets.response_datasets.geometry3k import Geometry3KDataset
+from nemo_rl.data.datasets.response_datasets.oai_format_dataset import (
+    OpenAIFormatDataset,
+)
+from nemo_rl.data.datasets.response_datasets.oasst import OasstDataset
+from nemo_rl.data.datasets.response_datasets.openmathinstruct2 import (
+    OpenMathInstruct2Dataset,
+)
+from nemo_rl.data.datasets.response_datasets.refcoco import RefCOCODataset
+from nemo_rl.data.datasets.response_datasets.response_dataset import ResponseDataset
+from nemo_rl.data.datasets.response_datasets.squad import SquadDataset
+
+
+def load_response_dataset(data_config, seed: int):
+    """Loads response dataset."""
+    dataset_name = (
+        data_config["dataset_name"] if "dataset_name" in data_config else "from_json"
+    )
+
+    # TODO @yukih: remove duplicated dataset_name (openmathinstruct2, clevr_cogent)
+    # for sft training
+    if dataset_name == "open_assistant":
+        base_dataset = OasstDataset(
+            output_dir="/tmp/open_assistant",
+            seed=seed,
+        )
+    elif dataset_name == "squad":
+        base_dataset = SquadDataset()
+    elif dataset_name == "openmathinstruct2":
+        base_dataset = OpenMathInstruct2Dataset(
+            split=data_config["split"],
+            output_key=data_config["output_key"],
+            prompt_file=data_config["prompt_file"],
+            seed=seed,
+        )
+    elif dataset_name == "clevr_cogent":
+        base_dataset = CLEVRCoGenTDataset(
+            split=data_config["split"],
+            prompt_file=data_config["prompt_file"],
+        )
+    elif dataset_name == "openai_format":
+        base_dataset = OpenAIFormatDataset(
+            data_config["train_data_path"],
+            data_config["val_data_path"],
+            data_config["chat_key"],
+            data_config["system_key"],
+            data_config["system_prompt"],
+        )
+    # for rl training
+    elif dataset_name == "OpenMathInstruct-2":
+        print("Loading nvidia/OpenMathInstruct2Dataset for training and validation")
+        base_dataset: Any = OpenMathInstruct2Dataset(seed=seed)
+    elif dataset_name == "DeepScaler":
+        print(
+            "Loading agentica-org/DeepScaleR-Preview-Dataset for training and validation"
+        )
+        base_dataset: Any = DeepScalerDataset(seed=seed)
+    # for vlm rl training
+    elif dataset_name == "clevr-cogent":
+        base_dataset: Any = CLEVRCoGenTDataset(
+            split=data_config["split"],
+        )
+    elif dataset_name == "refcoco":
+        base_dataset: Any = RefCOCODataset(
+            split=data_config["split"],
+            download_dir=data_config["download_dir"],
+        )
+    elif dataset_name == "geometry3k":
+        base_dataset: Any = Geometry3KDataset(
+            split=data_config["split"],
+        )
+    # fall back to load from JSON file
+    else:
+        base_dataset = ResponseDataset(
+            data_config["train_data_path"],
+            data_config["val_data_path"],
+            data_config["input_key"],
+            data_config["output_key"],
+        )
+
+    return base_dataset
+
+
+__all__ = [
+    "CLEVRCoGenTDataset",
+    "DeepScalerDataset",
+    "Geometry3KDataset",
+    "OpenAIFormatDataset",
+    "OasstDataset",
+    "OpenMathInstruct2Dataset",
+    "RefCOCODataset",
+    "ResponseDataset",
+    "SquadDataset",
+]
