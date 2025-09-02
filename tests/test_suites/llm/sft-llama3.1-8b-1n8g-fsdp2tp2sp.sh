79ab15661c1ff017a6e92e17915c1419bc7c4669
Ahmad Kiswani <kiswani.ahmad@gmail.com>
2025-09-03 00:49:07 +0300

    first attempt to test run times

--- tests/test_suites/llm/sft-llama3.1-8b-1n8g-fsdp2tp2sp.sh
+++ tests/test_suites/llm/sft-llama3.1-8b-1n8g-fsdp2tp2sp.sh
@@ -7,7 +7,7 @@ NUM_NODES=1
 STEPS_PER_RUN=50
 MAX_STEPS=50
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=45
+NUM_MINUTES=200
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
