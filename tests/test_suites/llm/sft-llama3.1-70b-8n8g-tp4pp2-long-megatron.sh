7e19af808deff130092ec0e69c9d5f00074d2ae7
Ahmad Kiswani <kiswani.ahmad@gmail.com>
2025-09-03 00:49:07 +0300

    tuning test suit run times
    Signed-off-by: Ahmad Kiswani <kiswani.ahmad@gmail.com>

--- tests/test_suites/llm/sft-llama3.1-70b-8n8g-tp4pp2-long-megatron.sh
+++ tests/test_suites/llm/sft-llama3.1-70b-8n8g-tp4pp2-long-megatron.sh
@@ -7,7 +7,7 @@ NUM_NODES=8
 STEPS_PER_RUN=300
 MAX_STEPS=300
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=240
+NUM_MINUTES=50
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
