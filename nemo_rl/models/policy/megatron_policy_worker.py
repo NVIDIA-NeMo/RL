ff7cc237b1d590c8ad736878a38e7d79dd0d6840
Kate Cheng <yunhsuanc@nvidia.com>
2025-09-16 13:21:55 -0700

    Add empty_unused_memory_level for empty_cache()
    Signed-off-by: Kate Cheng <yunhsuanc@nvidia.com>

--- nemo_rl/models/policy/megatron_policy_worker.py
+++ nemo_rl/models/policy/megatron_policy_worker.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import gc
 import os
 import time
 import warnings
@@ -513,7 +514,7 @@ class MegatronPolicyWorker:
             )
 
         cfg_from_pretrained = ConfigContainer.from_yaml(
-            pretrained_run_config, mode=InstantiationMode.STRICT
+            pretrained_run_config, mode=InstantiationMode.LENIENT
         )
         model_cfg = cfg_from_pretrained.model
         cfg_from_pretrained.logger = LoggerConfig()
@@ -978,6 +979,10 @@ class MegatronPolicyWorker:
                         do_not_average_loss=True,
                     )
 
+                # Empty unused memory.
+                if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+                    torch.cuda.empty_cache()
+
                 # Update parameters.
                 if not eval_mode:
                     update_successful, grad_norm, num_zeros_in_grad = (
@@ -1004,6 +1009,10 @@ class MegatronPolicyWorker:
                     skipped_iter = 0
                 else:
                     skipped_iter = 1
+
+                # Empty unused memory.
+                if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 2:
+                    torch.cuda.empty_cache()
 
                 if parallel_state.is_pipeline_last_stage(ignore_virtual=True):
                     # keep all microbatch metrics to be normalized later
@@ -1297,6 +1306,10 @@ class MegatronPolicyWorker:
 
                 # - self.model is the original reference_model, now on CUDA
                 # - self.reference_model is the original model, now on CPU
+
+                if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+                    gc.collect()
+                    torch.cuda.empty_cache()
                 yield
 
             finally:
@@ -1306,6 +1319,10 @@ class MegatronPolicyWorker:
                 # if isinstance(item, torch.Tensor):
                 # item = item.detach().to(device="cuda", non_blocking=True, copy=True)
                 # self.model.state_dict()[name] = item
+
+                if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+                    gc.collect()
+                    torch.cuda.empty_cache()
 
                 ## re-enable overlap param gather after weight swap
                 if self.should_disable_forward_pre_hook:
@@ -1699,6 +1716,9 @@ class MegatronPolicyWorker:
                     if torch.is_tensor(v) and not v.is_cuda:
                         state[k] = v.to("cuda")
 
+        if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+            torch.cuda.empty_cache()
+
     @wrap_with_nvtx_name("megatron_policy_worker/offload_before_refit")
     def offload_before_refit(self):
         """Offload the optimizer and buffers to the CPU."""
@@ -1727,6 +1747,10 @@ class MegatronPolicyWorker:
                         # Move the tensor to CPU and update the state dictionary
                         state[k] = v.to("cpu")
 
+        if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+            gc.collect()
+            torch.cuda.empty_cache()
+
         # Print memory stats after offloading
         allocated = torch.cuda.memory_allocated() / (1024**3)  # Convert to GB
         reserved = torch.cuda.memory_reserved() / (1024**3)  # Convert to GB
@@ -1749,6 +1773,9 @@ class MegatronPolicyWorker:
             del self._held_gather_buffer
             self._held_gather_buffer = None
 
+        if self.cfg["megatron_cfg"]["empty_unused_memory_level"] >= 1:
+            gc.collect()
+            torch.cuda.empty_cache()
         allocated = torch.cuda.memory_allocated() / (1024**3)  # Convert to GB
         reserved = torch.cuda.memory_reserved() / (1024**3)  # Convert to GB
         print(
