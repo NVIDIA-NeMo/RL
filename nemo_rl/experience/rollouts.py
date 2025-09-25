d34d84f5e49bdcece375f92b46fe935ffb0051fe
slikhite-1 <slikhite@nvidia.com>
2025-09-24 17:19:33 -0700

    rollouts updated
    Signed-off-by: slikhite-1 <slikhite@nvidia.com>

--- nemo_rl/experience/rollouts.py
+++ nemo_rl/experience/rollouts.py
@@ -492,8 +492,6 @@ def run_multi_turn_rollout(
     
     #append next user question for BFCL environments (those with user_question_bank)
     for global_idx in continuing_indices_global.tolist():
-        print("global_idx", global_idx)
-        print("current batch", current_batch)
         meta = current_batch["extra_env_info"][global_idx]
         if isinstance(meta, dict) and "user_question_bank" in meta:
             _append_next_user_question(
@@ -503,7 +501,6 @@ def run_multi_turn_rollout(
                 max_seq_len,
                 sample_token_counts,
             )
-        print("current batch after", current_batch)
     # Record samples that reached max turns
     sample_max_turns_reached[active_indices] = True
 
