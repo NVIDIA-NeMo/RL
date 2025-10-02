55687a30d6a68080cf3573c4ed80e75ab7e1185d
Terry Kong <terryk@nvidia.com>
2025-10-02 05:38:06 +0000

    pr538 squash
    Co-authored-by: Dheeraj Peri <peri.dheeraj@gmail.com>
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- nemo_rl/algorithms/dpo.py
+++ nemo_rl/algorithms/dpo.py
@@ -151,6 +151,8 @@ def setup(
     # ==========================
     #      Checkpointing
     # ==========================
+    # Add policy model name to checkpointing config
+    master_config["checkpointing"]["_model_name"] = policy_config["model_name"]
     checkpointer = CheckpointManager(master_config["checkpointing"])
     last_checkpoint_path = checkpointer.get_latest_checkpoint_path()
     dpo_save_state: Optional[DPOSaveState] = cast(
