4124c58fd7c7b7035a2ae035b7653c87229bf581
Anna Shors <ashors@nvidia.com>
2025-10-29 15:39:08 -0700

    fix functional tests
    Signed-off-by: Anna Shors <ashors@nvidia.com>

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -1191,7 +1191,8 @@ def grpo_train(
                         assert full_metric_name.startswith(
                             "train:"
                         ) or full_metric_name.startswith("val:"), (
-                            f"metric_name={full_metric_name} must start with 'val:' or 'train:'.\n"
+                            f"metric_name={full_metric_name} must start with 'val:' or 'train:',\n"
+                            f"followed by the corresponding name in the \"val\" or \"train\" metrics dictionary."
                             f"  If you are using an old config, please updated checkpointing.metric_name to the new format, "
                             f" e.g. 'val_reward --> 'val:reward'"
                         )
@@ -2039,7 +2040,7 @@ def async_grpo_train(
                             "train:"
                         ) or full_metric_name.startswith("val:"), (
                             f"metric_name={full_metric_name} must start with 'val:' or 'train:',\n"
-                            f"followed by the corresponding name in the "val" or "train" metrics dictionary."
+                            f"followed by the corresponding name in the \"val\" or \"train\" metrics dictionary."
                             f"  If you are using an old config, please updated checkpointing.metric_name to the new format, "
                             f" e.g. 'val_reward --> 'val:accuracy'"
                         )
--- nemo_rl/algorithms/rm.py
+++ nemo_rl/algorithms/rm.py
@@ -584,7 +584,7 @@ def rm_train(
                             "train:"
                         ) or full_metric_name.startswith("val:"), (
                             f"metric_name={full_metric_name} must start with 'val:' or 'train:',\n"
-                            f"followed by the corresponding name in the "val" or "train" metrics dictionary."
+                            f"followed by the corresponding name in the \"val\" or \"train\" metrics dictionary."
                             f"  If you are using an old config, please updated checkpointing.metric_name to the new format, "
                             f" e.g. 'val_loss --> 'val:validation-default_loss'"
                         )
