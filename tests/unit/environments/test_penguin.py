26a83039a509b7a980b387f3914c6d9ff5772997
Brian Yu <bxyu@nvidia.com>
2025-11-01 17:20:28 -0700

    dont convert
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- tests/unit/environments/test_penguin.py
+++ tests/unit/environments/test_penguin.py
@@ -163,8 +163,9 @@ def test_penguin_sanity(penguin, penguin_sanity_test_data, penguin_vllm_generati
     for d in actual_result:
         for message in d["input_message_log"]:
             message["token_ids"] = message["token_ids"].tolist()
-        for message in d["message_log"][:1]:
-            message["token_ids"] = message["token_ids"].tolist()
+        # Right now, we don't need to swap the token ids in the message log since they pointto the same underlying dictionary as above.
+        # for message in d["message_log"][:1]:
+        #     message["token_ids"] = message["token_ids"].tolist()
 
     def _standardize_single_result(d: dict):
         d = deepcopy(d)
