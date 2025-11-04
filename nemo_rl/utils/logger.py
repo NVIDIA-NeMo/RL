8469ec3995ffcdeeb9f789a38eae2352d63b8457
Brian Yu <bxyu@nvidia.com>
2025-11-03 20:57:08 -0800

    add print
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_rl/utils/logger.py
+++ nemo_rl/utils/logger.py
@@ -167,6 +167,7 @@ class WandbLogger(LoggerInterface):
         self.run = wandb.init(**cfg, dir=log_dir)
 
         if os.environ.get("RAY_BACKEND_LOG_LEVEL", None).lower() == "debug":
+            print("Uploading raylet.out and raylet.err files to W&B since environment variable RAY_BACKEND_LOG_LEVEL=debug")
             wandb.save("/tmp/ray/session_latest/logs/raylet.out", policy="live")
             wandb.save("/tmp/ray/session_latest/logs/raylet.err", policy="live")
 
