cf067eb7cf7514a765373bd3a368c4ae6ac51765
Brian Yu <bxyu@nvidia.com>
2025-11-04 21:49:10 -0800

    print
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_rl/distributed/batched_data_dict.py
+++ nemo_rl/distributed/batched_data_dict.py
@@ -485,14 +485,16 @@ class BatchedDataDict(UserDict, Generic[DictT]):
                 num_bins = len(all_chunk_bin_assignments[chunk_idx])
                 chunk_start = chunk_idx * batch_size
                 for bin_idx in range(num_bins):
-                    with timer.time("iterate bins / select"):
                         shard_idx = bin_idx % shards
+                    with timer.time("iterate bins / select / init"):
                         bin_indices = all_chunk_bin_assignments[chunk_idx][bin_idx]
                         global_bin_indices = [i + chunk_start for i in bin_indices]
+                    with timer.time("iterate bins / select / select_indices"):
                         sharded_data[shard_idx].append(
                             self.select_indices(global_bin_indices)
                         )
                         global_indices_per_shard[shard_idx].extend(global_bin_indices)
+                    with timer.time("iterate bins / select / bin_seqlen"):
                         bin_seqlen = sum(
                             [
                                 _get_padded_seqlen(input_lens[i].item())
@@ -500,17 +502,16 @@ class BatchedDataDict(UserDict, Generic[DictT]):
                             ]
                         )
 
-                    with timer.time("iterate bins / apply"):
-                        if chunk_sharded_micro_indices[shard_idx] == []:
-                            chunk_sharded_micro_indices[shard_idx].append(
-                                [0, len(bin_indices)]
-                            )
-                        else:
-                            prev_bin_end = chunk_sharded_micro_indices[shard_idx][-1][1]
-                            chunk_sharded_micro_indices[shard_idx].append(
-                                [prev_bin_end, prev_bin_end + len(bin_indices)]
-                            )
-                        chunk_sharded_micro_lengths[shard_idx].append(bin_seqlen)
+                    if chunk_sharded_micro_indices[shard_idx] == []:
+                        chunk_sharded_micro_indices[shard_idx].append(
+                            [0, len(bin_indices)]
+                        )
+                    else:
+                        prev_bin_end = chunk_sharded_micro_indices[shard_idx][-1][1]
+                        chunk_sharded_micro_indices[shard_idx].append(
+                            [prev_bin_end, prev_bin_end + len(bin_indices)]
+                        )
+                    chunk_sharded_micro_lengths[shard_idx].append(bin_seqlen)
 
                 for shard_idx in range(shards):
                     sharded_micro_indices[shard_idx].append(
@@ -532,11 +533,10 @@ class BatchedDataDict(UserDict, Generic[DictT]):
             for shard_idx in range(shards):
                 with timer.time("aggregate shards / from batches"):
                     shard = SlicedDataDict.from_batches(sharded_data[shard_idx])
-                with timer.time("aggregate shards / set"):
-                    shard.micro_batch_indices = sharded_micro_indices[shard_idx]
-                    shard.micro_batch_lengths = sharded_micro_lengths[shard_idx]
-                    shard.elem_counts_per_gb = sharded_elem_counts_per_gb[shard_idx]
-                    aggregated_shards.append(shard)
+                shard.micro_batch_indices = sharded_micro_indices[shard_idx]
+                shard.micro_batch_lengths = sharded_micro_lengths[shard_idx]
+                shard.elem_counts_per_gb = sharded_elem_counts_per_gb[shard_idx]
+                aggregated_shards.append(shard)
 
             print("Sharded data dict times taken:", timer.get_timing_metrics("sum"))
 
