71f738ceb9690b580285ea5f5a6feac94e7b544f
Mahan Fathi <mfathi@nvidia.com>
2025-11-13 08:54:17 -0800

    Make server logs verbose

--- xp/examples/run_llada_eval_pipeline_preset_cpu_partition.sh
+++ xp/examples/run_llada_eval_pipeline_preset_cpu_partition.sh
@@ -24,6 +24,7 @@ export SERVER_MODEL_PATH="nvidia/Nemotron-Diffusion-Research-4B-v0"
 export SERVER_BASE_MODEL="nvidia/Nemotron-Diffusion-Research-4B-v0"
 #export SERVER_DCP_PATH="/lustre/fsw/portfolios/llmservice/users/degert/results/diffusion_sft-reasoning_off_identity_fix_math_new_r1_strict-filter_holdout_rl_15percent-OCI-4b-nvidia-diffusion-qwen3-epochs-3-gbs-256-lr-2.5e6-lambda/step_1100/policy"
 export SERVER_ENGINE="nemotron"
+export SERVER_EXTRA_ARGS="--verbose"
 
 # Run EVAL on cpu partition (attempt same node via --use-same-node)
 export SEQ_EVAL_PARTITION=$PARTITION
