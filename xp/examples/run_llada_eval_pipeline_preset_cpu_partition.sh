86efd81feba8f730b54f9bc5db1ff25679956e15
Mahan Fathi <mfathi@nvidia.com>
2025-11-13 07:36:52 -0800

    Fix partition

--- xp/examples/run_llada_eval_pipeline_preset_cpu_partition.sh
+++ xp/examples/run_llada_eval_pipeline_preset_cpu_partition.sh
@@ -14,16 +14,19 @@
 
 set -euo pipefail
 
+export PARTITION="batch_singlenode"
+
 # Run SERVER on interactive partition (REQUIRED for GPU access)
-export SERVER_PARTITION="interactive"
+export SERVER_PARTITION=$PARTITION
 export SERVER_GPUS=8
 export SERVER_BATCH_SIZE=1
+export SERVER_MODEL_PATH="nvidia/Nemotron-Diffusion-Research-4B-v0"
 export SERVER_BASE_MODEL="nvidia/Nemotron-Diffusion-Research-4B-v0"
-export SERVER_DCP_PATH="/lustre/fsw/portfolios/llmservice/users/degert/results/diffusion_sft-reasoning_off_identity_fix_math_new_r1_strict-filter_holdout_rl_15percent-OCI-4b-nvidia-diffusion-qwen3-epochs-3-gbs-256-lr-2.5e6-lambda/step_1100/policy"
+#export SERVER_DCP_PATH="/lustre/fsw/portfolios/llmservice/users/degert/results/diffusion_sft-reasoning_off_identity_fix_math_new_r1_strict-filter_holdout_rl_15percent-OCI-4b-nvidia-diffusion-qwen3-epochs-3-gbs-256-lr-2.5e6-lambda/step_1100/policy"
 export SERVER_ENGINE="nemotron"
 
 # Run EVAL on cpu partition (attempt same node via --use-same-node)
-export SEQ_EVAL_PARTITION="cpu"
+export SEQ_EVAL_PARTITION=$PARTITION
 export SEQ_EVAL_BENCHMARK="gsm8k:1"
 export SEQ_EVAL_GENERATION_ALGORITHM="nemotron"
 export SEQ_EVAL_THRESHOLD="0.9"
