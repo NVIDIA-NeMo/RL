ae5d7ac6bc84f0c522f94fbcbcb9eb09c7e0a3d7
Ahmad Kiswani <kiswani.ahmad@gmail.com>
2025-11-17 20:51:58 +0200

    test metrics
    Signed-off-by: Ahmad Kiswani <kiswani.ahmad@gmail.com>

--- tests/test_suites/llm/grpo-qwen3-next-80ba3b-8n8g-megatron.sh
+++ tests/test_suites/llm/grpo-qwen3-next-80ba3b-8n8g-megatron.sh
@@ -31,11 +31,10 @@ uv run examples/run_grpo_math.py \
 # Convert tensorboard logs to json
 uv run tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
 
-# Only run metrics if the target step is reached
-# TODO(ahmadki): set metrics
-# if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
-#     uv run tests/check_metrics.py $JSON_METRICS \
-#         'data["train/token_mult_prob_error"]["30"] < 1.1' \
-#         'data["train/reward"]["30"] > 0.43' \
-#         'mean(data["timing/train/total_step_time"], -6, -1) < 220'
-# fi
+Only run metrics if the target step is reached
+if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
+    uv run tests/check_metrics.py $JSON_METRICS \
+        'data["train/token_mult_prob_error"]["30"] < 1.1' \
+        'data["train/reward"]["30"] > 0.43' \
+        'mean(data["timing/train/total_step_time"], -6, -1) < 305'
+fi
