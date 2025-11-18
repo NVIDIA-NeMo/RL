da0c361a90616aee1ad11d3cd495220083975a39
Seonjin Na <sna@nvidia.com>
2025-11-17 18:07:56 -0800

    result summary scripts

---
+++ get_wandb_log_for_nemorl.py
@@ -0,0 +1,61 @@
+
+import wandb
+import argparse
+import os
+#import pprint
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--org", type=str, default="nvidia")
+    parser.add_argument("--project", type=str, default="nemo-rl")
+    parser.add_argument("--uid", type=str, required=True)
+    parser.add_argument("--at-step", type=int, default=5)
+    parser.add_argument("--average-steps", type=int, default=5)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    assert os.environ.get("WANDB_API_KEY") is not None, "WANDB_API_KEY is not set"
+
+    keys = [
+        "train/mean_total_tokens_per_sample",
+        "timing/train/total_step_time",
+        "timing/train/exposed_generation",
+        # "timing/train/generation",
+        "timing/train/policy_and_reference_logprobs",
+        "timing/train/policy_training",
+        "timing/train/weight_sync", 
+        "timing/train/data_processing",
+        "timing/train/reward_calculation",
+        "timing/train/training_prep",
+
+
+        # "timing/train/prepare_for_generation/total",
+        # "timing/train/prepare_for_generation/transfer_and_update_weights",
+        "performance/tokens_per_sec_per_gpu",
+        "performance/generation_tokens_per_sec_per_gpu",
+        "performance/training_worker_group_tokens_per_sec_per_gpu",
+        "performance/policy_training_tokens_per_sec_per_gpu",
+        "performance/policy_and_reference_logprobs_tokens_per_sec_per_gpu",
+        "performance/train_flops_per_gpu",
+        "performance/train_fp_utilization",
+    ]
+
+    api = wandb.Api()
+    run = api.run(f"{args.org}/{args.project}/{args.uid}")
+    min_step = args.at_step - args.average_steps // 2
+    max_step = args.at_step + args.average_steps // 2 - 1 + (args.average_steps % 2)
+    history = run.history().loc[min_step:max_step, keys]
+
+    # get average of the history
+    average_history = history.mean(axis=0)
+    history_at_step = history.loc[args.at_step]
+    print("Average history:")
+    #pprint.pprint(average_history)
+    print(average_history)
+    print("History at step:")
+    #pprint.pprint(history_at_step)
+    print(history_at_step)
+
+if __name__ == "__main__":
+    main()
