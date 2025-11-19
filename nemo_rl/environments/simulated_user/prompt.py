52faa8c05918f4da30be98e472c7a9fa8fb292ee
Mohammadreza Mohseni <mmohseni@google.com>
2025-11-19 21:49:12 +0000

    Added copyright notice
    Signed-off-by: Mohammadreza Mohseni <mmohseni@google.com>

--- nemo_rl/environments/simulated_user/prompt.py
+++ nemo_rl/environments/simulated_user/prompt.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 starting_user_prompt = (
     "I will play a game with you. I have a list of integers in mind and can NOT tell you. "
     "Your goal is to guess the count of UNIQUE numbers in my list. The only 2 things you can do is the following: "
