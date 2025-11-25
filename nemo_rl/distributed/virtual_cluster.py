6fbbbb741e680ee2d020d9f73063aa831a8f7e9d
Zhuoran Yin <yzr1914001753@gmail.com>
2025-11-25 21:14:33 +0000

    sglang: add sglang setup in grpo.py, add find available port to set up servers
    Signed-off-by: Ryan <yzr1914001753@gmail.com>
    Signed-off-by: Zhuoran Yin <yzr1914001753@gmail.com>

--- nemo_rl/distributed/virtual_cluster.py
+++ nemo_rl/distributed/virtual_cluster.py
@@ -57,6 +57,8 @@ class PY_EXECUTABLES:
 
     # Use NeMo-Gym dependencies
     NEMO_GYM = f"uv run --locked --extra nemo_gym --directory {git_root}"
+    # Use NeMo-RL direct dependencies and SGLang.
+    SGLANG = "uv run --locked --extra sglang --directory {git_root}"
 
 
 @ray.remote  # pragma: no cover
