225c7156836c8a18ab537247e5849f4f9bcd8377
Yuanhang Su <yuanhangs@nvidia.com>
2025-12-12 11:02:58 -0800

    use decord for video and audio loading
    Signed-off-by: yuanhangs <yuanhangs@nvidia.com>

--- nemo_rl/data/llm_message_utils.py
+++ nemo_rl/data/llm_message_utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import warnings
+import decord
 from PIL import Image
 from collections import defaultdict
 from typing import Any, Optional, Union, cast
