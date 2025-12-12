ec210e509594274d77e44e336ffc3f708b0f3c36
Yuanhang Su <yuanhangs@draco-oci-login-01.cm.cluster>
2025-12-12 13:12:52 -0800

    video output bugfix
    Signed-off-by: yuanhangs <yuanhangs@nvidia.com>

--- nemo_rl/data/llm_message_utils.py
+++ nemo_rl/data/llm_message_utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import warnings
+import time
 import decord
 from PIL import Image
 from collections import defaultdict
