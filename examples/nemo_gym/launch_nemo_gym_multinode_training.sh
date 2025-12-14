d778e16d9c1f7f798e718db31924b0afded88bc0
Brian Yu <bxyu@nvidia.com>
2025-12-14 11:05:04 -0800

    add submit script
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

---
+++ examples/nemo_gym/launch_nemo_gym_multinode_training.sh
@@ -0,0 +1,34 @@
+# ----- PARAMETERS -----
+# WANDB_API_KEY, EXP_NAME, NUM_ACTOR_NODES, REPO_LOCATION
+
+# ----- CONSTANTS -----
+CONTAINER_IMAGE_PATH=/path/to/nemo-rl/container
+
+read -r -d '' COMMAND <<EOF
+cd ${REPO_LOCATION}
+
+HF_HOME=$PWD/.cache/ \
+HF_HUB_OFFLINE=1 \
+WANDB_API_KEY=$WANDB_API_KEY \
+NRL_FORCE_REBUILD_VENVS=true \
+uv run python examples/nemo_gym/run_grpo_nemo_gym.py \
+    cluster.num_nodes=$NUM_ACTOR_NODES \
+    logger.wandb.name=$EXP_NAME \
+    logger.log_dir=results/$EXP_NAME \
+    checkpointing.checkpoint_dir=results/$EXP_NAME \
+    $@
+EOF
+
+echo -e "Running command:\n$COMMAND"
+
+# ray.sub needs to be launched from the NeMo-RL root directory
+cd $REPO_LOCATION
+COMMAND=$COMMAND \
+CONTAINER=$CONTAINER_IMAGE_PATH \
+MOUNTS="/shared/filesystem:/shared/filesystem" \
+sbatch \
+    --nodes=$NUM_ACTOR_NODES \
+    --time=4:0:0 \
+    --job-name=$EXP_NAME \
+    --gres=gpu:8 \
+    ray.sub
