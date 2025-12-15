860b48dc52ed5a618987e9a91962a76bdcec2079
Brian Yu <bxyu@nvidia.com>
2025-12-14 21:32:16 -0800

    rebuild venvs again
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- examples/nemo_gym/launch_nemo_gym_multinode_training.sh
+++ examples/nemo_gym/launch_nemo_gym_multinode_training.sh
@@ -8,12 +8,9 @@ cd $REPO_LOCATION
 read -r -d '' COMMAND <<EOF
 cd ${REPO_LOCATION}
 
-source /opt/nemo_rl_venv/bin/activate
-uv sync --group={build,docs,dev,test} --extra nemo_gym
-uv run nemo_rl/utils/prefetch_venvs.py
-
 HF_HOME=$PWD/.cache/ \
 WANDB_API_KEY=$WANDB_API_KEY \
+NRL_FORCE_REBUILD_VENVS=true \
 uv run python examples/nemo_gym/run_grpo_nemo_gym.py \
     ++cluster.num_nodes=$NUM_ACTOR_NODES \
     ++logger.wandb.name=$EXP_NAME \
