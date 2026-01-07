c5bca9419a54909294c2181b501e603d0c8db497
Terry Kong <terrycurtiskong@gmail.com>
2026-01-07 02:04:47 -0800

    fix: fix several nightly tests that were flaky (#1724)
    Signed-off-by: Terry Kong <terryk@nvidia.com>
    Signed-off-by: yuanhangs <yuanhangs@nvidia.com>

--- tests/test_suites/llm/performance/grpo-qwen3-235b-32n8g-async-1off.sh
+++ tests/test_suites/llm/performance/grpo-qwen3-235b-32n8g-async-1off.sh
@@ -24,6 +24,7 @@ uv run examples/run_grpo_math.py \
     logger.wandb.project=nemo-rl \
     logger.wandb.name=$EXP_NAME \
     logger.monitor_gpus=True \
+    logger.tensorboard_enabled=True \
     checkpointing.enabled=True \
     checkpointing.checkpoint_dir=$CKPT_DIR \
     $@ \
