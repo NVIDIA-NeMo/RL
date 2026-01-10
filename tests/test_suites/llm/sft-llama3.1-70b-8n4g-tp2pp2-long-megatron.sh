d16ef81c044d3df68fbbed689992513c3ae7ba22
Charlie Truong <chtruong@nvidia.com>
2026-01-10 03:20:12 +0000

    Update gb200 test times
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- tests/test_suites/llm/sft-llama3.1-70b-8n4g-tp2pp2-long-megatron.sh
+++ tests/test_suites/llm/sft-llama3.1-70b-8n4g-tp2pp2-long-megatron.sh
@@ -8,7 +8,7 @@ GPUS_PER_NODE=4
 STEPS_PER_RUN=300
 MAX_STEPS=300
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=240
+NUM_MINUTES=170
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
--- tests/test_suites/llm/sft-llama3.2-1b-1n4g-fsdp2tp1.v3.sh
+++ tests/test_suites/llm/sft-llama3.2-1b-1n4g-fsdp2tp1.v3.sh
@@ -8,7 +8,7 @@ GPUS_PER_NODE=4
 STEPS_PER_RUN=250
 MAX_STEPS=250
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=15
+NUM_MINUTES=25
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
