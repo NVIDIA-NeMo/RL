2b26cd9b7d7c20248255314a89009fe2be521e79
Charlie Truong <chtruong@nvidia.com>
2026-01-10 02:17:25 +0000

    Revert "5 steps"
    This reverts commit 8ca7edc1164f5b1c843650c11a7cbb7f7a8cdc07.
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- tests/test_suites/llm/sft-qwen2.5-math7b-2n4g-megatron.sh
+++ tests/test_suites/llm/sft-qwen2.5-math7b-2n4g-megatron.sh
@@ -8,8 +8,8 @@ source $SCRIPT_DIR/common.env
 # ===== BEGIN CONFIG =====
 NUM_NODES=2
 GPUS_PER_NODE=4
-STEPS_PER_RUN=5
-MAX_STEPS=5
+STEPS_PER_RUN=80  # step_time ~ 29sec
+MAX_STEPS=80
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
 NUM_MINUTES=30
 # ===== END CONFIG =====
