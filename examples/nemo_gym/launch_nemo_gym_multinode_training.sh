7969ab170b77c41f263660f5e773f6f8b3d4ada1
Brian Yu <bxyu@nvidia.com>
2026-01-12 15:36:41 -0800

    add HF TOKEN
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- examples/nemo_gym/launch_nemo_gym_multinode_training.sh
+++ examples/nemo_gym/launch_nemo_gym_multinode_training.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # ----- PARAMETERS -----
-# WANDB_API_KEY, EXP_NAME, NUM_ACTOR_NODES, NUM_SLURM_NODES (optional), REPO_LOCATION, CONTAINER_IMAGE_PATH, SLURM_ACCOUNT, SLURM_PARTITION
+# WANDB_API_KEY, HF_TOKEN, EXP_NAME, NUM_ACTOR_NODES, NUM_SLURM_NODES (optional), REPO_LOCATION, CONTAINER_IMAGE_PATH, SLURM_ACCOUNT, SLURM_PARTITION
 
 # ray.sub needs to be launched from the NeMo-RL root directory
 cd $REPO_LOCATION
@@ -23,6 +23,7 @@ read -r -d '' COMMAND <<EOF
 cd ${REPO_LOCATION}
 
 HF_HOME=$PWD/.cache/ \
+HF_TOKEN=$HF_TOKEN \
 WANDB_API_KEY=$WANDB_API_KEY \
 uv run python examples/nemo_gym/run_grpo_nemo_gym.py \
     ++cluster.num_nodes=$NUM_ACTOR_NODES \
