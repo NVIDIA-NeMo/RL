b1f1b5d18d46abfe5b9fba0a651743256dc8efbb
ruit <ruit@nvidia.com>
2026-01-14 20:01:22 -0800

    fix functional test
    Signed-off-by: ruit <ruit@nvidia.com>

--- tests/functional/grpo_automodel_lora.sh
+++ tests/functional/grpo_automodel_lora.sh
@@ -24,18 +24,18 @@ cd $PROJECT_ROOT
 uv run coverage run -a --data-file=$PROJECT_ROOT/tests/.coverage --source=$PROJECT_ROOT/nemo_rl \
     $PROJECT_ROOT/examples/run_grpo_math.py\
     grpo.max_num_steps=3 \
-    grpo.num_prompts_per_step=32 \
-    grpo.num_generations_per_prompt=16 \
+    grpo.num_prompts_per_step=8 \
+    grpo.num_generations_per_prompt=4 \
     policy.dtensor_cfg.lora_cfg.enabled=True \
     policy.dtensor_cfg.lora_cfg.dim=32 \
-    +policy.generation.vllm_cfg.skip_tokenizer_init=false \
-    policy.dtensor_cfg.tensor_parallel_size=1 \
-    policy.train_global_batch_size=512 \
-    policy.train_micro_batch_size=4 \
-    logger.wandb_enabled=False \
-    checkpointing.enabled=false \
+    policy.train_global_batch_size=32 \
+    policy.train_micro_batch_size=1 \
     cluster.gpus_per_node=2 \
-    checkpointing.checkpoint_dir=/tmp/lora_grpo_checkpoints \
+    logger.tensorboard_enabled=true \
+    logger.log_dir=$LOG_DIR \
+    logger.wandb_enabled=false \
+    logger.monitor_gpus=true \
+    checkpointing.enabled=false \
     "$@" \
     2>&1 | tee $RUN_LOG
 
