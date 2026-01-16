21d71dc5b8fd3ae248840ebcc57387f62a182c9f
Anna Shors <ashors@nvidia.com>
2026-01-16 12:56:06 -0800

    add missing import
    Signed-off-by: Anna Shors <ashors@nvidia.com>

--- nemo_rl/models/megatron/config.py
+++ nemo_rl/models/megatron/config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, NamedTuple, Optional, TypeVar
+from typing import Any, Callable, NamedTuple, Optional, TypedDict, TypeVar
 
 import torch
 from megatron.bridge.training.config import ConfigContainer
