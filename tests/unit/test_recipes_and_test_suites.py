26d83b6e4e79677f1c4696dcd3c979205553dd7f
jianh <jianh@nvidia.com>
2026-02-02 10:25:43 -0800

    fix
    Signed-off-by: jianh <jianh@nvidia.com>

--- tests/unit/test_recipes_and_test_suites.py
+++ tests/unit/test_recipes_and_test_suites.py
@@ -216,7 +216,7 @@ def test_all_recipe_yamls_accounted_for_in_test_suites(
     )
 
 
-def test_nightly_compute_stays_below_1210_hours(nightly_test_suite, tracker):
+def test_nightly_compute_stays_below_1250_hours(nightly_test_suite, tracker):
     command = f"DRYRUN=1 HF_HOME=... HF_DATASETS_CACHE=... CONTAINER= ACCOUNT= PARTITION= ./tools/launch {' '.join(nightly_test_suite)}"
 
     print(f"Running command: {command}")
@@ -248,8 +248,8 @@ def test_nightly_compute_stays_below_1210_hours(nightly_test_suite, tracker):
         f"Last line of output was not as expected: '{last_line}'"
     )
     total_gpu_hours = float(last_line.split(":")[-1].strip())
-    assert total_gpu_hours <= 1210, (
-        f"Total GPU hours exceeded 1210: {last_line}. We should revisit the test suites to reduce the total GPU hours."
+    assert total_gpu_hours <= 1250, (
+        f"Total GPU hours exceeded 1250: {last_line}. We should revisit the test suites to reduce the total GPU hours."
     )
     tracker.track("total_nightly_gpu_hours", total_gpu_hours)
 
