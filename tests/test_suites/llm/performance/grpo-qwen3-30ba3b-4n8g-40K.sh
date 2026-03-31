beab63c9b9fcb53e136ef424b40ead40a25210dc
Yuki Huang <yukih@nvidia.com>
2026-03-31 06:05:37 -0700

    update test time
    - grpo-llama3.1-8b-instruct-4n8g-fsdp2tp1-long.v3
    - grpo-qwen2.5-32b-32n8g-fsdp2tp8-actckpt-long.v3
    - grpo-qwen2.5-32b-32n8g-fsdp2tp8-actckpt.v3
    - grpo-qwen3-30ba3b-4n8g-40K
    
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

--- tests/test_suites/llm/performance/grpo-qwen3-30ba3b-4n8g-40K.sh
+++ tests/test_suites/llm/performance/grpo-qwen3-30ba3b-4n8g-40K.sh
@@ -9,7 +9,7 @@ NUM_NODES=4
 STEPS_PER_RUN=5
 MAX_STEPS=10
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=100
+NUM_MINUTES=240
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
