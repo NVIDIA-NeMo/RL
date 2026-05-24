61fdf3741eff93b9b7de18cdf0a7f8b822210929
bg51717 <biguo@nvidia.com>
2026-05-23 19:05:29 -0700

    add fast label to functional test
    Signed-off-by: bg51717 <biguo@nvidia.com>

--- tests/functional/L1_Functional_Tests_GPU.sh
+++ tests/functional/L1_Functional_Tests_GPU.sh
@@ -71,7 +71,7 @@ if python -c "import sglang" 2>/dev/null; then
     run_test      uv run --no-sync bash ./tests/functional/grpo_sglang.sh
 fi
 run_test fast uv run --no-sync bash ./tests/functional/grpo_topp_topk.sh
-run_test      uv run --no-sync bash ./tests/functional/ppo_megatron.sh
+run_test fast uv run --no-sync bash ./tests/functional/ppo_megatron.sh
 run_test      uv run --no-sync bash ./tests/functional/prorlv2.sh
 run_test      uv run --no-sync bash ./tests/functional/qa_distillation_megatron.sh
 run_test      uv run --no-sync bash ./tests/functional/rm.sh
