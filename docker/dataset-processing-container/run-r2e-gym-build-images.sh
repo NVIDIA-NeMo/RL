11f0924197dfac57cc32d0b9a06f190b27bed140
Hristo Filaretov <hfilaretov@nvidia.com>
2026-06-18 10:00:12 +0200

    use repo

--- docker/dataset-processing-container/run-r2e-gym-build-images.sh
+++ docker/dataset-processing-container/run-r2e-gym-build-images.sh
@@ -5,7 +5,8 @@ STATE_DIR=${STATE_DIR:-/workspace/state}
 MAX_WORKERS=${MAX_WORKERS:-1}
 INSTANCE_FILE="${1:-/workspace/r2e-gym-instances-to-build.txt}"
 
-uv run --with datasets python /workspace/build_r2e_gym_images.py \
+cd /workspace/repos/r2e-gym-arm-build
+uv run --with datasets python src/r2egym/repo_analysis/build_arm64_dockers.py \
     --instance-file "${INSTANCE_FILE}" \
     --state-file "${STATE_DIR}/r2e_gym_build_push_state.json" \
     --max-workers "${MAX_WORKERS}" \
