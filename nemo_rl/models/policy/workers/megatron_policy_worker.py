d2a24cbbcb11d08e64423979c1bc95a194d84017
ℍ𝕠𝕝𝕝𝕠𝕨 𝕄𝕒𝕟 <j88437182@hotmail.com>
2026-06-25 16:33:30 -0700

    feat(modelopt): support real NVFP4 QAT rollout (#2592)
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- nemo_rl/models/policy/workers/megatron_policy_worker.py
+++ nemo_rl/models/policy/workers/megatron_policy_worker.py
@@ -1366,6 +1366,9 @@ class MegatronPolicyWorkerImpl(
             post_iter_func=lambda x: x[1],
         )
 
+    def _use_real_quant_refit(self) -> bool:
+        return False
+
     def prepare_for_lp_inference(self):
         self.model = self.move_model(self.model, "cuda", move_grads=False)
         self.model.eval()
