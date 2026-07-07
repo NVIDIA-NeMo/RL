8ee53bef1534b85a67815be90223c753f22611ae
Yuki Huang <yukih@nvidia.com>
2026-07-07 06:24:57 -0700

    update metrics
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

--- tests/test_suites/llm/grpo-qwen2.5-1.5B-4n8g-megatron-yarn-256k.sh
+++ tests/test_suites/llm/grpo-qwen2.5-1.5B-4n8g-megatron-yarn-256k.sh
@@ -7,7 +7,7 @@ NUM_NODES=4
 STEPS_PER_RUN=30
 MAX_STEPS=30
 NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
-NUM_MINUTES=60
+NUM_MINUTES=90
 # ===== END CONFIG =====
 
 exit_if_max_steps_reached
@@ -34,8 +34,8 @@ uv run tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
 if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
     uv run tests/check_metrics.py $JSON_METRICS \
         'mean(data["train/token_mult_prob_error"], ignore_top_p=0.05) < 1.05' \
-        'mean(data["train/reward"], -6, -1) > 0.0' \
-        'data["validation/accuracy"]["30"] > 0.18'
+        'data["train/reward"]["30"] > 0.5' \
+        'data["validation/accuracy"]["30"] > 0.1'
 
     # Clean up checkpoint directory after successful run to save space.
     rm -rf "$CKPT_DIR"
