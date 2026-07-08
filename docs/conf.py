1f885a442acba0a5065e9268e20e4ae968975815
botbikamordehai2-sketch <botbikamordehai2@gmail.com>
2026-07-08 11:13:03 +0000

    fix: complete truncated html_theme_options in conf.py (closes #2816)

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,8 @@ html_theme_options = {
         "json_url": "../versions1.json",
         "version_match": release,
     },
-    "extra_head": {
+    # Add extra head content here (e.g., analytics scripts)
+": {
         """
     <script src="https://assets.adobedtm.com/5d4962a43b79/c1061d2c5e7b/launch-191c2462b890.min.js" ></script>
     """
