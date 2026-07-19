3475d166f99f3ec217b4657c4ff52c093dc7fb72
Yuki Huang <yukih@nvidia.com>
2026-07-18 21:38:20 -0700

    update nightly threshold
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

--- tests/test_suites/llm/grpo-llama3.1-8b-instruct-2n8g-async-1off-single-controller-streaming2.sh
+++ tests/test_suites/llm/grpo-llama3.1-8b-instruct-2n8g-async-1off-single-controller-streaming2.sh
@@ -35,7 +35,8 @@ uv run tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
 if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
     uv run tests/check_metrics.py $JSON_METRICS \
         'median(data["train/token_mult_prob_error"]) < 1.1' \
-        'data["train/token_mult_prob_error"]["10"] < 1.1'
+        'data["train/token_mult_prob_error"]["10"] < 1.1' \
+        'mean(data["train/grad_norm"], 2, 0) > 0.10'
 
     # Clean up checkpoint directory after successful run to save space.
     rm -rf "$CKPT_DIR"
