13267b9a8b9130236a01d186a380d30aed58e4bb
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2026-07-29 12:35:53 -0500

    Update tests/functional/L1_Functional_Tests_Megatron_4.sh
    Co-authored-by: Terry Kong <terrycurtiskong@gmail.com>
    Signed-off-by: Teodor-Dumitru Ene <teodord.ene@gmail.com>

--- tests/functional/L1_Functional_Tests_Megatron_4.sh
+++ tests/functional/L1_Functional_Tests_Megatron_4.sh
@@ -54,7 +54,7 @@ if megatron_generation_supported; then
     run_test      uv run --no-sync bash ./tests/functional/grpo_megatron_generation_async.sh
     run_test fast uv run --no-sync bash ./tests/functional/grpo_megatron_generation_colocated_async.sh
     run_test      uv run --no-sync bash ./tests/functional/grpo_megatron_generation_async_gym.sh
-    run_test      uv run --no-sync bash ./tests/functional/grpo_megatron_generation_topp_topk.sh
+    run_test fast uv run --no-sync bash ./tests/functional/grpo_megatron_generation_topp_topk.sh
     run_test      uv run --no-sync bash ./tests/functional/grpo_megatron_generation_multiturn.sh
 fi
 
