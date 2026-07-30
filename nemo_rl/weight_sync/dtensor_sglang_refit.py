Kh4L/NemoRL

fix(sglang): keep the SGLang refit dispatch out of the policy workers
`_refit_sglang_dispatch` and `SGLangColocatedWeightSynchronizer._refit_colocated`
run in the driver process but imported `megatron_policy_worker` /
`dtensor_policy_worker_v2` to reach `refit_sglang_{colocated,distributed}`.
Those modules import `megatron.bridge` and `nemo_automodel` at module scope,
and the driver environment is synced without the `mcore` / `automodel` extras,
which is why worker imports otherwise go through `PY_EXECUTABLES`. Every SGLang
refit therefore failed on its first attempt with
`ModuleNotFoundError: No module named 'megatron'`, reproduced on a two node run.

Move the four drivers to `nemo_rl/weight_sync/`, next to the synchronizer that
calls them, and repoint both dispatch sites. The function bodies and names are
unchanged, and the dispatch keeps its lazy `as _backend` import, so each call
site changes only the module it imports from. The drivers call `policy` and
`policy_generation` facade methods plus `ray`, and already imported
`nemo_rl.models.policy.utils` lazily, so they need no extra.

The test parses the sources instead of importing them, so it also fails in an
environment that provides a backend, such as the Megatron unit test shards.

Signed-off-by: Serge Panev <spanev@nvidia.com>

nemo_rl/weight_sync/dtensor_sglang_refit.py
@@ -1,0 +1,97 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Driver-side SGLang refit drivers for the DTensor policy backend.
+
+These run in the driver process, which is synced without a training-backend
+extra, so they cannot live beside code that imports ``megatron.bridge`` or
+``nemo_automodel`` at module scope. They only drive the ``policy`` and
+``policy_generation`` facades.
+"""
+
+from typing import Any
+
+import ray
+
+
+def refit_sglang_colocated(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Refit colocated SGLang engines from the FSDP/DTensor policy.
+
+    Lifecycle: optional fault-tolerance recover, connect (when new /
+    recovered engines), pause + KV invalidation, send HF tensor buckets via
+    Ray IPC, post-process, continue. Mirrors the Megatron colocated driver;
+    the FSDP path is BF16-only.
+    """
+    from nemo_rl.models.policy.utils import fetch_updatable_engines_with_recover
+
+    (
+        rollout_engines,
+        _rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines(
+            engine_gpu_counts=engine_gpu_counts,
+            engine_gpu_offsets=engine_gpu_offsets,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    policy_generation.invalidate_kv_cache()
+    try:
+        futures = policy.update_weights_to_sglang_colocated(
+            rollout_engines=rollout_engines,
+            buffer_size_bytes=buffer_size_bytes,
+        )
+        ray.get(futures)
+        policy_generation.post_process_weights()
+    finally:
+        policy_generation.continue_generation()
+    return True
+
+
+def refit_sglang_distributed(
+    *,
+    policy: Any,  # noqa: ARG001 — accepted for dispatch parity
+    policy_generation: Any,  # noqa: ARG001
+    buffer_size_bytes: int,  # noqa: ARG001
+) -> bool:
+    """SGLang disaggregate broadcast is not currently supported for FSDP.
+
+    Per the design, only the Megatron backend implements the distributed
+    refit path (it depends on AutoBridge restoring full HF tensors on
+    trainer rank 0). FSDP non-colocated refits should keep using the
+    legacy ``broadcast_weights_for_collective`` flow with a non-SGLang
+    generation backend.
+    """
+    raise NotImplementedError(
+        "SGLang weight_transfer_mode='broadcast' is currently only supported "
+        "for the Megatron policy backend."
+    )

nemo_rl/weight_sync/megatron_sglang_refit.py
@@ -1,0 +1,149 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Driver-side SGLang refit drivers for the Megatron policy backend.
+
+These run in the driver process, which is synced without a training-backend
+extra, so they cannot live beside code that imports ``megatron.bridge`` or
+``nemo_automodel`` at module scope. They only drive the ``policy`` and
+``policy_generation`` facades.
+"""
+
+from typing import Any
+
+import ray
+
+
+def refit_sglang_colocated(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Refit colocated SGLang engines from the Megatron policy.
+
+    Lifecycle: optional fault-tolerance recover, connect (when new /
+    recovered engines), pause + flush, send HF tensor buckets via Ray
+    IPC, post-process, continue.
+    """
+    from nemo_rl.models.policy.utils import (
+        fetch_updatable_engines_with_recover,
+        get_sglang_quantization_cfg,
+    )
+
+    sglang_quant = get_sglang_quantization_cfg(policy_generation)
+    target_precision = sglang_quant.get("scheme", "bf16")
+
+    (
+        rollout_engines,
+        _rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines(
+            engine_gpu_counts=engine_gpu_counts,
+            engine_gpu_offsets=engine_gpu_offsets,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    policy_generation.invalidate_kv_cache()
+    try:
+        # Per-worker actor method is now synchronous (per-chunk ray.get +
+        # lifetime-safe IPC handled inside send_hf_buckets_via_ipc_actor_impl),
+        # but the policy-group dispatch still returns one Ray future per
+        # worker; we await those here to wait for all trainer ranks.
+        futures = policy.update_weights_to_sglang_colocated(
+            rollout_engines=rollout_engines,
+            buffer_size_bytes=buffer_size_bytes,
+            target_precision=target_precision,
+            sglang_quantization_cfg=sglang_quant,
+        )
+        ray.get(futures)
+        policy_generation.post_process_weights()
+    finally:
+        policy_generation.continue_generation()
+    return True
+
+
+def refit_sglang_distributed(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Broadcast Megatron-restored HF tensors to disaggregate SGLang via NCCL.
+
+    Trainer rank 0 owns the SGLang weight-update group; non-rank-0 ranks still
+    walk the AutoBridge collective inside ``update_weights_to_sglang_distributed``
+    but do not broadcast. Includes optional fault-tolerance recover prelude.
+    """
+    from nemo_rl.models.policy.utils import (
+        fetch_updatable_engines_with_recover,
+        get_sglang_quantization_cfg,
+    )
+
+    sglang_quant = get_sglang_quantization_cfg(policy_generation)
+    target_precision = sglang_quant.get("scheme", "bf16")
+
+    (
+        rollout_engines,
+        rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        _engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines_distributed(
+            rollout_engines=rollout_engines,
+            engine_gpu_counts=engine_gpu_counts,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    if pause_mode != "in_place":
+        policy_generation.invalidate_kv_cache()
+    try:
+        futures = policy.update_weights_to_sglang_distributed(
+            rollout_engines=rollout_engines,
+            rollout_engine_lock=rollout_engine_lock,
+            buffer_size_bytes=buffer_size_bytes,
+            target_precision=target_precision,
+            sglang_quantization_cfg=sglang_quant,
+        )
+        ray.get(futures)
+        policy_generation.post_process_weights()
+    finally:
+        policy_generation.continue_generation()
+    return True

NVIDIA-NeMo/RL

nemo_rl/weight_sync/dtensor_sglang_refit.py
@@ -1,0 +1,102 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Driver-side SGLang refit drivers for the DTensor policy backend.
+
+These run in the driver process, which is synced without a training-backend
+extra, so they cannot live beside code that imports ``megatron.bridge`` or
+``nemo_automodel`` at module scope. They only drive the ``policy`` and
+``policy_generation`` facades.
+"""
+
+from typing import Any
+
+import ray
+
+
+def refit_sglang_colocated(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Refit colocated SGLang engines from the FSDP/DTensor policy.
+
+    Lifecycle: optional fault-tolerance recover, connect (when new /
+    recovered engines), pause + KV invalidation, send HF tensor buckets via
+    Ray IPC, post-process, continue. Mirrors the Megatron colocated driver;
+    the FSDP path is BF16-only.
+    """
+    from nemo_rl.models.policy.utils import fetch_updatable_engines_with_recover
+
+    (
+        rollout_engines,
+        _rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines(
+            engine_gpu_counts=engine_gpu_counts,
+            engine_gpu_offsets=engine_gpu_offsets,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    if pause_mode != "in_place":
+        policy_generation.invalidate_kv_cache()
+
+    policy_generation.begin_weight_update()
+    try:
+        futures = policy.update_weights_to_sglang_colocated(
+            rollout_engines=rollout_engines,
+            buffer_size_bytes=buffer_size_bytes,
+        )
+        ray.get(futures)
+    finally:
+        # Close the session and resume on every path, so a failed refit
+        # leaves the engine usable instead of wedged in the update state.
+        policy_generation.end_weight_update()
+        policy_generation.continue_generation()
+    return True
+
+
+def refit_sglang_distributed(
+    *,
+    policy: Any,  # noqa: ARG001 — accepted for dispatch parity
+    policy_generation: Any,  # noqa: ARG001
+    buffer_size_bytes: int,  # noqa: ARG001
+) -> bool:
+    """SGLang disaggregate broadcast is not currently supported for FSDP.
+
+    Per the design, only the Megatron backend implements the distributed
+    refit path (it depends on AutoBridge restoring full HF tensors on
+    trainer rank 0). FSDP non-colocated refits should keep using the
+    legacy ``broadcast_weights_for_collective`` flow with a non-SGLang
+    generation backend.
+    """
+    raise NotImplementedError(
+        "SGLang weight_transfer_mode='broadcast' is currently only supported "
+        "for the Megatron policy backend."
+    )

nemo_rl/weight_sync/megatron_sglang_refit.py
@@ -1,0 +1,158 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Driver-side SGLang refit drivers for the Megatron policy backend.
+
+These run in the driver process, which is synced without a training-backend
+extra, so they cannot live beside code that imports ``megatron.bridge`` or
+``nemo_automodel`` at module scope. They only drive the ``policy`` and
+``policy_generation`` facades.
+"""
+
+from typing import Any
+
+import ray
+
+
+def refit_sglang_colocated(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Refit colocated SGLang engines from the Megatron policy.
+
+    Lifecycle: optional fault-tolerance recover, connect (when new /
+    recovered engines), pause + flush, send HF tensor buckets via Ray
+    IPC, post-process, continue.
+    """
+    from nemo_rl.models.policy.utils import (
+        fetch_updatable_engines_with_recover,
+        get_sglang_quantization_cfg,
+    )
+
+    sglang_quant = get_sglang_quantization_cfg(policy_generation)
+    target_precision = sglang_quant.get("scheme", "bf16")
+
+    (
+        rollout_engines,
+        _rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines(
+            engine_gpu_counts=engine_gpu_counts,
+            engine_gpu_offsets=engine_gpu_offsets,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    if pause_mode != "in_place":
+        policy_generation.invalidate_kv_cache()
+
+    policy_generation.begin_weight_update()
+    try:
+        # Per-worker actor method is now synchronous (per-chunk ray.get +
+        # lifetime-safe IPC handled inside send_hf_buckets_via_ipc_actor_impl),
+        # but the policy-group dispatch still returns one Ray future per
+        # worker; we await those here to wait for all trainer ranks.
+        futures = policy.update_weights_to_sglang_colocated(
+            rollout_engines=rollout_engines,
+            buffer_size_bytes=buffer_size_bytes,
+            target_precision=target_precision,
+            sglang_quantization_cfg=sglang_quant,
+        )
+        ray.get(futures)
+    finally:
+        # Close the session and resume on every path, so a failed refit
+        # leaves the engine usable instead of wedged in the update state.
+        policy_generation.end_weight_update()
+        policy_generation.continue_generation()
+    return True
+
+
+def refit_sglang_distributed(
+    *,
+    policy: Any,
+    policy_generation: Any,
+    buffer_size_bytes: int,
+) -> bool:
+    """Broadcast Megatron-restored HF tensors to disaggregate SGLang via NCCL.
+
+    Trainer rank 0 owns the SGLang weight-update group; non-rank-0 ranks still
+    walk the AutoBridge collective inside ``update_weights_to_sglang_distributed``
+    but do not broadcast. Includes optional fault-tolerance recover prelude.
+    """
+    from nemo_rl.models.policy.utils import (
+        fetch_updatable_engines_with_recover,
+        get_sglang_quantization_cfg,
+    )
+
+    sglang_quant = get_sglang_quantization_cfg(policy_generation)
+    target_precision = sglang_quant.get("scheme", "bf16")
+
+    (
+        rollout_engines,
+        rollout_engine_lock,
+        num_new_engines,
+        engine_gpu_counts,
+        _engine_gpu_offsets,
+    ) = fetch_updatable_engines_with_recover(policy_generation)
+
+    if num_new_engines > 0:
+        policy.connect_sglang_rollout_engines_distributed(
+            rollout_engines=rollout_engines,
+            engine_gpu_counts=engine_gpu_counts,
+        )
+        policy_generation.clear_updatable_num_new_engines()
+        assert policy_generation.num_new_engines == 0, (
+            "clear_updatable_num_new_engines did not zero num_new_engines"
+        )
+
+    # Pause with the configured mode, but only invalidate the KV cache when
+    # the mode actually drops generation state. "in_place" leaves the engine
+    # paused without dropping its KV cache, so flushing would clobber the
+    # still-valid in-place state.
+    pause_mode = policy_generation.pause_generation_mode
+    policy_generation.pause_generation(mode=pause_mode)
+    if pause_mode != "in_place":
+        policy_generation.invalidate_kv_cache()
+
+    policy_generation.begin_weight_update()
+    try:
+        futures = policy.update_weights_to_sglang_distributed(
+            rollout_engines=rollout_engines,
+            rollout_engine_lock=rollout_engine_lock,
+            buffer_size_bytes=buffer_size_bytes,
+            target_precision=target_precision,
+            sglang_quantization_cfg=sglang_quant,
+        )
+        ray.get(futures)
+    finally:
+        # Close the session and resume on every path, so a failed refit
+        # leaves the engine usable instead of wedged in the update state.
+        policy_generation.end_weight_update()
+        policy_generation.continue_generation()
+    return True