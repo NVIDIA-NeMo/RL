d3727fe1cf16c07dfff463fd53ae9997397343bc
ashors1 <ashors@nvidia.com>
2026-08-05 14:06:26 -0700

    add missing imports
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- tests/unit/experience/test_rollouts.py
+++ tests/unit/experience/test_rollouts.py
@@ -39,7 +39,8 @@ from nemo_rl.environments.games.sliding_puzzle import (
     SlidingPuzzleMetadata,
 )
 from nemo_rl.experience.metric_utils import calculate_single_metric, pct
-from nemo_rl.experience.rollout_manager import AsyncNemoGymRolloutImpl
+from nemo_rl.experience.interfaces import Completion, PromptGroupRecord
+from nemo_rl.experience.rollout_manager import AsyncNemoGymRolloutImpl, RolloutManager
 from nemo_rl.experience.rollouts import (
     _tokenize_env_observation,
     generate_responses_async,
