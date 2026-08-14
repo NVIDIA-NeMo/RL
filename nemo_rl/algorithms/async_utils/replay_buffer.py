d681e0bba5cfe3ad671c76dfd66554cded42c077
Anish Mahishi <amahishi@nvidia.com>
2026-08-13 21:18:54 -0700

    fix: lint issues
    Signed-off-by: Anish Mahishi <amahishi@nvidia.com>
    (cherry picked from commit 976b2a6ab476b3532944975f53c5308a9de97687)

--- nemo_rl/algorithms/async_utils/replay_buffer.py
+++ nemo_rl/algorithms/async_utils/replay_buffer.py
@@ -29,7 +29,7 @@ from typing import Any, Iterable, Literal, Optional, TypedDict
 import ray
 
 from nemo_rl.algorithms.async_utils.interfaces import ReplayBufferProtocol
-from nemo_rl.data_plane import DATA_PLANE_CHECKPOINT_SCHEMA_VERSION, KVBatchMeta
+from nemo_rl.data_plane import KVBatchMeta
 from nemo_rl.data_plane.async_utils import call_data_plane
 from nemo_rl.data_plane.schema import ROUTE_PLAN_TAG, ROUTED_EXPERTS_FIELD
 from nemo_rl.experience.interfaces import PromptGroupRecord
