0d2ada94f574ed4ee161045f029c00b564035ff4
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2026-09-02 23:07:40 -0500

    lint
    Signed-off-by: Teodor-Dumitru Ene <teodord.ene@gmail.com>

--- nemo_rl/experience/rollout_manager.py
+++ nemo_rl/experience/rollout_manager.py
@@ -417,7 +417,11 @@ class _Deadline:
 
     def suspend(self) -> None:
         """Disarm the clock, banking whatever budget is left."""
-        if self._timeout is None or self._remaining is not None or self._timeout.expired():
+        if (
+            self._timeout is None
+            or self._remaining is not None
+            or self._timeout.expired()
+        ):
             return
         when = self._timeout.when()
         if when is None:
