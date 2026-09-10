2297b18b4a24bf59730f8daf78b23ddcba76a2cd
Laura Dang <lauradang.2000@gmail.com>
2026-09-10 02:38:01 -0400

    Update nemo_rl/models/generation/megatron/megatron_generation.py
    Signed-off-by: Laura Dang <lauradang.2000@gmail.com>

--- nemo_rl/models/generation/megatron/megatron_generation.py
+++ nemo_rl/models/generation/megatron/megatron_generation.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-import threading
 from copy import deepcopy
 from typing import TYPE_CHECKING, Any, AsyncGenerator, Optional, cast
 
