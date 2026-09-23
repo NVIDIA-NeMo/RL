ffadb853a32ac42fd9b917ff82278cb5b1ea454b
Carlos Gomes <cgomes@nvidia.com>
2026-09-23 10:42:32 +0200

    add todo comment
    Signed-off-by: Carlos Gomes <cgomes@nvidia.com>

--- nemo_rl/models/policy/workers/megatron_policy_worker.py
+++ nemo_rl/models/policy/workers/megatron_policy_worker.py
@@ -2378,6 +2378,8 @@ class MegatronPolicyWorkerImpl(
 
         no_grad.__exit__(None, None, None)
         self.timer.stop("get_logprobs")
+        
+        # TODO: @nan: will remove in the future
         cpu_logprobs = torch.empty_like(
             logprobs,
             device="cpu",
